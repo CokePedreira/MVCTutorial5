637253cfe7317bfc3ac38ec7f73b116d2d26615d
Jorge Pedreira <jorge.pedreira@gruposolutio.com>
2018-04-11 13:32:01 +0200

    Primer commit

---
+++ MVCTutorial5/Controllers/ProductController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using MVCTutorial5.Models;
+
+namespace MVCTutorial5.Controllers
+{
+    [Authorize]
+    public class ProductController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            string currentUserId = User.Identity.GetUserId();
+            var userProducts = db.Products.Where(p => p.UserId == currentUserId).ToList();
+            return View(userProducts);
+        }
+
+            // GET: Product/Details/5
+        public ActionResult Details(int? id)
+        {
+            Product product = db.Products.Find(id);
+            string currentUserId = User.Identity.GetUserId();
+            if ((product.UserId != currentUserId) || (product == null))
+            {
+                return HttpNotFound();
+            }
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductID,Description,Brand,Quantity,Price")] Product product)
+        {
+            string currentUserId = User.Identity.GetUserId();
+            product.UserId = currentUserId;
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Product/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductID,Description,Brand,Quantity,Price")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            string currentUserId = User.Identity.GetUserId();
+            if ((product.UserId != currentUserId) || (product == null))
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product product = db.Products.Find(id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MVCTutorial5/Migrations/Configuration.cs
@@ -0,0 +1,53 @@
+namespace MVCTutorial5.Migrations
+{
+    using MVCTutorial5.Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MVCTutorial5.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(MVCTutorial5.Models.ApplicationDbContext context)
+        {
+
+            context.Products.AddOrUpdate(p => p.ProductID,
+              new Product
+              {
+                  ProductID = 1,
+                  Description = "TV",
+                  Brand = "Samsung",
+                  Quantity = 1,
+                  Price = 500,
+
+              },
+              new Product
+              {
+                  ProductID = 2,
+                  Description = "Móvil",
+                  Brand = "Apple",
+                  Quantity = 1,
+                  Price = 600,
+
+              }
+           );
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
